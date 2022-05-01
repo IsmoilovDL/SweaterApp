badc719014c878e973f0d7afe68d9b973a32e533
311 <ismoilov92@gmail.com>
2022-05-01 15:50:55 +0300

    Add subscribe function

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -39,6 +40,17 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    @ManyToMany
+    @JoinTable(name = "user_subscriptions",
+                joinColumns = {@JoinColumn(name = "channel_id")},
+                inverseJoinColumns = {@JoinColumn(name = "subscriber_id")})
+    private Set<User> subscribers=new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(name = "user_subscriptions",
+            joinColumns = {@JoinColumn(name = "subscriber_id")},
+            inverseJoinColumns = {@JoinColumn(name = "channel_id")})
+    private Set<User> subscriptions=new HashSet<>();
 
 
     public Long getId() {
@@ -144,5 +156,21 @@ public class User implements UserDetails {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    public Set<User> getSubscribers() {
+        return subscribers;
+    }
+
+    public void setSubscribers(Set<User> subscribers) {
+        this.subscribers = subscribers;
+    }
+
+    public Set<User> getSubscriptions() {
+        return subscriptions;
+    }
+
+    public void setSubscriptions(Set<User> subscriptions) {
+        this.subscriptions = subscriptions;
     }
 }
