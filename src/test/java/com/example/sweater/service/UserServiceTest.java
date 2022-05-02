74ebb0b42811b451364ae03f4d5ad2885b17f842
311 <ismoilov92@gmail.com>
2022-05-02 19:53:02 +0300

    Unit test. Testing addUserFail, activateUser, activateUserFail

--- src/test/java/com/example/sweater/service/UserServiceTest.java
+++ src/test/java/com/example/sweater/service/UserServiceTest.java
@@ -7,6 +7,8 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -31,9 +33,56 @@ class UserServiceTest {
     @Test
     void addUser() {
         User user = new User();
+        user.setEmail("test@test.tt");
         boolean isUserCreated= userService.addUser(user);
+
         Assert.assertTrue(isUserCreated);
         Assert.assertNotNull(user.getActiveCode());
         Assert.assertTrue(CoreMatchers.is(user.getRoles()).matches(Collections.singleton(Role.USER)));
+
+        Mockito.verify(userRepo, Mockito.times(1)).save(user);
+    }
+
+    @Test
+    public void addUserFailTest(){
+        User user=new User();
+        user.setUsername("John");
+        Mockito.doReturn(new User())
+                .when(userRepo)
+                .findByUsername("John");
+
+        boolean isUserCreated= userService.addUser(user);
+        Assert.assertFalse(isUserCreated);
+        Mockito.verify(userRepo, Mockito.times(0)).save(ArgumentMatchers.any(User.class));
+
+    }
+
+    @Test
+    public void activateUser(){
+
+        User user=new User();
+        user.setActiveCode("haveCode");
+
+        Mockito.doReturn(user)
+                .when(userRepo)
+                .findByActiveCode("activate");
+
+        boolean isUserActivate = userService.activateUser("activate");
+
+        Assert.assertTrue(isUserActivate);
+        Assert.assertNotNull(user.getActiveCode());
+
+        Mockito.verify(userRepo, Mockito.times(1)).save(user);
+
+    }
+
+    @Test
+    public void activateUserFailTest(){
+        boolean isUserActivate = userService.activateUser("activate me");
+
+        Assert.assertFalse(isUserActivate);
+        Mockito.verify(userRepo, Mockito.times(0)).save(ArgumentMatchers.any(User.class));
+
+
     }
 }
